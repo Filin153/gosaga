07aad4debe7e13e9e6401db2e38979c324836d0d
Dmitry Savchenko <dsavchenko@titan-it.pro>
2025-12-21 17:48:32 +0300

    drop coder

--- domain/domain.go
+++ domain/domain.go
@@ -1,6 +1,11 @@
 package domain
 
-import "time"
+import (
+	"bytes"
+	"encoding/gob"
+	"errors"
+	"time"
+)
 
 type TaskStatus string
 
@@ -61,4 +66,28 @@ type DLQTaskUpdate struct {
 	TimeMul        *int   `db:"time_mul"`
 	MaxAttempts    *int   `db:"max_attempts"`
 	HaveAttempts   *int   `db:"have_attempts"`
+}
+
+// EncodeSagaMsg serializes SagaMsg using Go's builtin gob.
+func EncodeSagaMsg(msg *SagaMsg) ([]byte, error) {
+	if msg == nil {
+		return nil, errors.New("nil saga msg")
+	}
+	var buf bytes.Buffer
+	if err := gob.NewEncoder(&buf).Encode(msg); err != nil {
+		return nil, err
+	}
+	return buf.Bytes(), nil
+}
+
+// DecodeSagaMsg deserializes SagaMsg encoded by EncodeSagaMsg.
+func DecodeSagaMsg(data []byte) (*SagaMsg, error) {
+	if len(data) == 0 {
+		return nil, errors.New("empty saga msg data")
+	}
+	var msg SagaMsg
+	if err := gob.NewDecoder(bytes.NewReader(data)).Decode(&msg); err != nil {
+		return nil, err
+	}
+	return &msg, nil
 }
